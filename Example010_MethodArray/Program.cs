281111138e17b2cc54e2656ae6af5fe422b8a5d7
Ilya Lux <iliya92@mail.ru>
2022-12-17 13:15:47 +0300

    add Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+
+int[] array = { 11, 24, 23, 74, 58, 15, 63, 67, 58 };
+int n = array.Length;
+int find = 58;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        WriteLine(index);
+        break;
+    }
+    index++; // index = index + 1
+}
