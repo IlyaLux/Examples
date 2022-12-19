ba0225dbe79b60ac621af084954fd7bf4cbf0a44
Ilya Lux <iliya92@mail.ru>
2022-12-19 20:53:15 +0300

    seminar2 added

---
+++ Sem2_Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(10, 100);
+WriteLine(number);
+WriteLine(number / 10>number % 10?number / 10:number % 10);
+
+
+// int a1 = number / 10;
+// int a2 = number % 10;
+// WriteLine(a1>a2?a1:a2);
+
+// if(a1>a2){
+//     WriteLine(a1);
+// }
+// else{
+//     WriteLine(a2);
+// }
---
+++ Sem2_Task0/sem2_task1/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+int a1 = number / 100;
+int a2 = number % 10;
+Write(a1);
+WriteLine(a2);
