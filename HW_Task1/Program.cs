8f1d318c669f00c59d7ae8e0be19c8276c2c7e7d
Ilya Lux <iliya92@mail.ru>
2022-12-19 19:51:09 +0300

    Первая задача первого дз

---
+++ HW_Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Нахождение максимального из двух чисел");
+WriteLine("Введите число 1");
+int a = int.Parse(ReadLine());
+WriteLine("Введите число 2");
+int b = int.Parse(ReadLine());
+if(a>b){
+    WriteLine($"Первое число {a} больше второго {b}");
+}
+if(a<b){
+    WriteLine($"Второе число {b} больше первого {a}");
+}
+if(a==b) {
+    WriteLine("Числа равны");
+}
