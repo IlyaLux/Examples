a3859e3bf635765ee077f8870227f9848b5fd5b7
Ilya Lux <iliya92@mail.ru>
2022-12-20 20:16:42 +0300

    HW2_task2 added

---
+++ HomeWork2/HW2_task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Вывод третьей цифры заданного числа.");
+WriteLine("Введите число:");
+int a = int.Parse(ReadLine());
+int b = 0;
+if (a > 99 || a < -99)
+{
+    while (a > 99 || a < -99)
+    {
+        b = a % 10;
+        a = a / 10;
+    }
+    WriteLine(b < 0 ? -b : b);
+}
+else WriteLine("третьей цифры нет");
