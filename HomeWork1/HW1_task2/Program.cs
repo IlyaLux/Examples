7be0b66493a71cc30b0da9c8998290ad82328a9f
Ilya Lux <iliya92@mail.ru>
2022-12-20 15:34:12 +0300

    HW1_task2 added

---
+++ HomeWork1/HW1_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Нахождение максимального из трех чисел");
+WriteLine("Введите число 1:");
+int a1 = int.Parse(ReadLine());
+WriteLine("Введите число 2:");
+int a2 = int.Parse(ReadLine());
+WriteLine("Введите число 3:");
+int a3 = int.Parse(ReadLine());
+int max = a1;
+if (a2 > max)
+{
+    max = a2;
+}
+if (a3 > max)
+{
+    max = a3;
+}
+WriteLine("Максимальное число "+ max);
