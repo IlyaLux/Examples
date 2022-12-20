5aaba7f33fa56d18b05fb2dfdc36e049a15e767d
Ilya Lux <iliya92@mail.ru>
2022-12-20 15:06:17 +0300

    sem2_tas0 replaced

--- Sem2_Task0/sem2_task1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using static System.Console;
-Clear();
-
-int number = new Random().Next(100, 1000);
-WriteLine(number);
-int a1 = number / 100;
-int a2 = number % 10;
-WriteLine(a1 * 10 + a2);
-// WriteLine(a2);
--- Sem2_Task0/sem2_task5/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using static System.Console;
-Clear();
-
-WriteLine("Первый стрелок сбил:");
-int a = int.Parse(ReadLine());
-WriteLine("Второй стрелок сбил:");
-int b = int.Parse(ReadLine());
-
-WriteLine("Всего сбито " + (a + b - 1) + " банок");
