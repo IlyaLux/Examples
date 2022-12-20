fd94afe1f4921ead3833e88682a30e11a3e65912
Ilya Lux <iliya92@mail.ru>
2022-12-20 15:17:01 +0300

    HW1 task1 modified

--- HW_Task1/Program.cs
+++ HW_Task1/Program.cs
@@ -6,12 +6,25 @@ WriteLine("Введите число 1");
 int a = int.Parse(ReadLine());
 WriteLine("Введите число 2");
 int b = int.Parse(ReadLine());
+
+if(a==b){
+    WriteLine("Числа равны");
+} 
 if(a>b){
     WriteLine($"Первое число {a} больше второго {b}");
 }
-if(a<b){
+else {
     WriteLine($"Второе число {b} больше первого {a}");
 }
-if(a==b) {
-    WriteLine("Числа равны");
-}
+
+// WriteLine(a==b ? "Числа равны" : a>b ? $"Первое число {a} больше второго {b}" : $"Второе число {b} больше первого {a}");
+
+// if(a>b){
+//     WriteLine($"Первое число {a} больше второго {b}");
+// }
+// if(a<b){
+//     WriteLine($"Второе число {b} больше первого {a}");
+// }
+// if(a==b) {
+//     WriteLine("Числа равны");
+// }
