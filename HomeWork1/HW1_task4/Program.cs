91f443c563a2259aba1b21a64084844ec5f54fd3
Ilya Lux <iliya92@mail.ru>
2022-12-20 16:52:20 +0300

    HW1_task4 added

---
+++ HomeWork1/HW1_task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Вывод четных чисел от 1 до заданного числа");
+WriteLine("Введите число:");
+int a = int.Parse(ReadLine());
+int b = 1;
+
+if (a > 1)
+{
+    while (b <= a)
+    {
+        if (b%2==0)
+        {
+            Write(b + " ");
+        }
+        b++;
+    }
+}
+else
+{
+    while (b >= a)
+    {
+        if (b%2==0)
+        {
+            Write(b + " ");
+        }
+        b--;
+    }
+}
+WriteLine();
