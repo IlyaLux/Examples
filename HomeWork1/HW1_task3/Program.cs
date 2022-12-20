a88383fb272ca852194796b2f7978a4447018d19
Ilya Lux <iliya92@mail.ru>
2022-12-20 15:49:21 +0300

    HW1_task3 added

---
+++ HomeWork1/HW1_task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Явяляется ли число четным?");
+WriteLine("Введите число:");
+int a = int.Parse(ReadLine());
+
+if (a % 2 == 0)
+{
+    WriteLine("число четное");
+}
+else
+{
+    WriteLine("число нечетное");
+}
+
+// WriteLine(a % 2 == 0 ? "число четное" : "число нечетное");
