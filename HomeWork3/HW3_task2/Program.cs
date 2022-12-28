8e1aa7d259e69bc35ecbbc1cde5b89383ed8aaec
Ilya Lux <iliya92@mail.ru>
2022-12-28 19:46:29 +0300

    HW3 task2 added

---
+++ HomeWork3/HW3_task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Найти расстояние между двумя точками в 3D пространстве.");
+WriteLine("Введите координаты точки a");
+int ax = int.Parse(ReadLine());
+int ay = int.Parse(ReadLine());
+int az = int.Parse(ReadLine());
+
+WriteLine("Введите координаты точки b");
+int bx = int.Parse(ReadLine());
+int by = int.Parse(ReadLine());
+int bz = int.Parse(ReadLine());
+
+WriteLine($"{Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2)):f2}");
