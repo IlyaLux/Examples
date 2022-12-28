4eff33f7098c8563d80d5b8eda4dbfec311759ef
Ilya Lux <iliya92@mail.ru>
2022-12-28 19:59:15 +0300

    HW3 task3 added

---
+++ HomeWork3/HW3_task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Найти кубы чисел от 1 до N.");
+WriteLine("Введите число:");
+if (!int.TryParse(ReadLine(), out int N))
+{
+    WriteLine("Введите число!");
+    return;
+}
+int b = 1;
+if (N > 0)
+{
+    while (b <= N)
+    {
+        Write(Math.Pow(b, 3) + " ");
+        b++;
+    }
+}
+else
+{
+    while (b >= N)
+    {
+        Write(Math.Pow(b, 3) + " ");
+        b--;
+    }
+}
