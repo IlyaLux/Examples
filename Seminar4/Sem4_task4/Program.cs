80b6be65de2c47a7426fe721ffe5d11003f611c6
Lux Ilya <iliya92@mail.ru>
2023-01-12 22:09:46 +0300

    task4,5 added

---
+++ Seminar4/Sem4_task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число");
+int B = int.Parse(ReadLine());
+int sum = summa(B);
+WriteLine(sum);
+
+
+
+
+int summa(int A)
+{
+    int result = 1;
+    int sum = 0;
+
+    while (result < A)
+    {
+        if (A % result = 0)
+        {
+            sum += result;
+        }
+
+        result++;
+    }
+    return result;
+}
---
+++ Seminar4/Sem4_task5/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+using static System.Console;
+Clear();
+
+int[] ar = GetArray();
+GetArray();
+PrintArray(ar);
+
+
+void FullArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+}
+
+
+
+    
+    
+        
+    
+
+
