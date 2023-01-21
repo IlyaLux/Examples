9ea61f892c0c3c1ad7e61de98123e834b527030b
Lux Ilya <iliya92@mail.ru>
2023-01-21 21:44:08 +0300

    HW5_task1 added

---
+++ HomeWork5/HW5_task1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу реализующую методы формирования массива, заполненного случайными положительными трёхзначными числами,
+// и подсчета количества чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите размер массива: ");
+if (!int.TryParse(ReadLine(), out int n) || n < 0)
+{
+    WriteLine("Введите целое, положительное число");
+    return;
+}
+
+int[] array = GetArray(n);
+PrintArray(array);
+WriteLine();
+WriteLine($"Количество четных чисел в массиве: {CountEven(array)}.");
+
+
+
+
+int[] GetArray(int size)
+{
+    int[] resultArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = new Random().Next(100, 1000);
+    }
+    return resultArray;
+}
+
+void PrintArray(int[] arrayName)
+{
+    for (int i = 0; i < arrayName.Length; i++)
+    {
+        Write($"{arrayName[i]} ");
+    }
+}
+
+int CountEven(int[] arrayName)
+{
+    int count = 0;
+    foreach (var a in arrayName)
+    {
+        if (a % 2 == 0) count++;
+    }
+    return count;
+}
