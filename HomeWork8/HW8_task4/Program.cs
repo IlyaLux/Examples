125f6539cc1006b6a4c0e676f611065433614ff1
Ilya Lux <iliya92@mail.ru>
2023-02-02 15:40:37 +0300

    HW8_task4 added

---
+++ HomeWork8/HW8_task4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+using static System.Console;
+Clear();
+
+int[,,] dddArray = {{{66,25},
+                     {34,41}},
+                    {{27,90},
+                     {26,55}}};
+
+PrintdddMatrix(dddArray);
+
+
+
+void PrintdddMatrix(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Write($"{inArray[i, j, k]} ({i},{j},{k}) ");
+            }
+            WriteLine();
+        }
+    }
+}
