06dac63e7653736b736ccef8091474267ac7f7ef
Lux Ilya <iliya92@mail.ru>
2023-02-18 20:58:24 +0300

    Program.cs added

---
+++ ChoiceSpecControl/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите массив строк ");
+
+string[] InString = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int count = 0;
+foreach (var item in InString)
+{
+    if (item.Length <= 3) count++;
+}
+
+if (count > 0)
+{
+    string[] resultArray = new string[count];
+    int num = 0;
+    for (int i = 0; i < InString.Length; i++)
+    {
+        if (InString[i].Length <= 3)
+        {
+            resultArray[num] = InString[i];
+            num++;
+        }
+    }
+    PrintArray(resultArray);
+}
+else
+{
+    WriteLine();
+}
+
+
+
+
+
+
+void PrintArray(string[] arrayName)
+{
+    for (int i = 0; i < arrayName.Length - 1; i++)
+    {
+        Write($"{arrayName[i]}, ");
+    }
+    Write(arrayName[arrayName.Length - 1]);
+}
